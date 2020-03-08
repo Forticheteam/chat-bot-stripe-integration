f4daa66e661cc33100a9de35a15fa0b477f3e043
J. Fernando González <jfgonzalezm@gmail.com>
2020-03-08 20:24:02 +0100

    echo endpoint to debug chatbot

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Flask"
+    return "Hello World from ... Flask"
+
+@app.route("/api/echo", methods=['POST'])
+def create_share_link():
+    return json.loads(request.data)
+    
+
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=5000)
