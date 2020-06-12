c2d95cc21db297a37ead9adb17e0680dd8c1236b
J. Fernando González <jfgonzalezm@gmail.com>
2020-06-12 11:35:53 +0200

    fix: take out www domain from letsencrypt

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(prescriptvm.com www.prescriptvm.com pagos.prescriptvm.com)
+domains=(pagos.prescriptvm.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="jfgonzalezm@gmail.com" # Adding a valid address is strongly recommended
